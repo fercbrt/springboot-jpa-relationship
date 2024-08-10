d875ea1532e1cdeba5ed2125fc9db259c54566c5
Fernando Calvino Balonero <fcalvino@alumnos.unex.es>
2024-08-10 09:09:53 +0200

    Initial commit

---
+++ src/test/java/es/fercbrt/springbootjparelationship/SpringbootJpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fercbrt.springbootjparelationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaRelationshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
